021123cd6fbc475b52cc9c177cc5491c9d3a1192
Amir <amir@Amirs-MacBook-Pro.local>
2026-08-23 23:34:52 +0200

    creates get task list endpoint

--- backend/src/main/java/com/amir/backend/services/TaskListService.java
+++ backend/src/main/java/com/amir/backend/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.amir.backend.services;
 import com.amir.backend.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList) throws IllegalAccessException;
+    Optional<TaskList> getTaskListById(UUID id);
 }
